d3a599386d09125ed00909a5496c58fb1bbde531
anushree-dataWiz <119417841+anushree-dataWiz@users.noreply.github.com>
2022-11-29 23:24:08 +1100

    Update streamlit_app.py
    Add menu items

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega3 and Blueberry Oatmeal')
+streamlit.text('Spinach, Kale and Rocket smoothie')
+streamlit.text('Hard-Boiled Free Range Egg')
