5d0eb2c94a36fda7d74ee15013696ea4e27dabc8
anushree-dataWiz <119417841+anushree-dataWiz@users.noreply.github.com>
2022-11-29 23:37:51 +1100

    Update streamlit_app.py
    Update breakfast menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@ import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
 streamlit.header('Breakfast Menu')
-streamlit.text('Omega3 and Blueberry Oatmeal')
-streamlit.text('Spinach, Kale and Rocket smoothie')
-streamlit.text('Hard-Boiled Free Range Egg')
+streamlit.text('🥣 Omega3 and Blueberry Oatmeal')
+streamlit.text('🥗Spinach, Kale and Rocket smoothie')
+streamlit.text('🐔Hard-Boiled Free Range Egg')
+streamlit.text('🥑🍞Avocado Toast')
